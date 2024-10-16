3477ac3d127258a8ea4983c2271997c4c6402cdc
Swarneem Nandanwar <swarneem.nandanwar@ascendion.com>
2024-10-16 15:42:29 +0530

    commit

---
+++ Labs/Registration_Form/datacapture.js
@@ -0,0 +1,15 @@
+function display(){
+    event.preventDefault(event);
+
+    var form = document.querySelector('form');
+
+    var First_Name = document.getElementById("fName").value;
+    var Last_Name = document.getElementById("lName").value;
+    var Contact = document.getElementById("phoneNO").value;    
+    var Email_ID = document.getElementById("email").value;
+    var Date_Of_Birth = document.getElementById("DOB").value;
+    var Gender = document.getElementById("Gender").value; 
+    var Weight = document.getElementById("weight").value;
+    var Package = document.getElementById("").value;
+    
+}
