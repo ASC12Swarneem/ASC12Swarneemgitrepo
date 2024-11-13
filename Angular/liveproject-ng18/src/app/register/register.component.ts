1bdfb9d452046cf09c976e86f37d695634806381
swar_neem <swarneemn@gmail.com>
2024-11-13 10:55:34 +0530

    search function completed

--- Angular/liveproject-ng18/src/app/register/register.component.ts
+++ Angular/liveproject-ng18/src/app/register/register.component.ts
@@ -4,19 +4,21 @@ import { Router } from '@angular/router';
 import { EmployeeService } from '../service/employee.service';
 import { CommonModule } from '@angular/common';
 
-@Component({
+@Component(
+  {
   selector: 'app-register',
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
-})
+  })
 
 export class RegisterComponent 
 {
   employeeForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,  private router: Router, private employeeService: EmployeeService){
+  constructor(private formBuilder: FormBuilder,  private router: Router, private employeeService: EmployeeService)
+  {
     this.employeeForm = this.formBuilder.group({
       id: ['', [Validators.required, Validators.pattern("^[0-9]*$")]], 
       name: ['', [Validators.required, Validators.minLength(3)]], 
@@ -25,29 +27,35 @@ export class RegisterComponent
   }
 
   ngOnInit() {
-    this.employeeForm = this.formBuilder.group({
+    this.employeeForm = this.formBuilder.group(
+    {
         id: [],
         name: [],
         salary: []
-    }
-    );
+    });
 }
 
 
-onSubmit() {
-  if (this.employeeForm.valid) {
+onSubmit() 
+{
+  if (this.employeeForm.valid) 
+    {
     const employeeData = this.employeeForm.value;
 
     console.log('Employee registered:', employeeData);
     alert("Registered successfully")
 
-    this.employeeService.createEmployee(employeeData).subscribe({
-        next:(_Response) =>{
+    this.employeeService.createEmployee(employeeData).subscribe(
+      {
+        next:(_Response) =>
+        {
           this.router.navigate(['/employees']);
         }
     })
 
-  } else {
+  } 
+  else 
+  {
     console.log('Form is invalid');
   }
 }
