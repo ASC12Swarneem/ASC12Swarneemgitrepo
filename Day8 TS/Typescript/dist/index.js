ee253f19d48a9399f841183fca53133710ee1882
swar_neem <swarneemn@gmail.com>
2024-11-18 10:38:26 +0530

    Removing files

--- Day8 TS/Typescript/dist/index.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello world, I am learning typescript");
-var message = "All JS files are TS by default";
-console.log(message);
-//Static Typing
-function add(a, b) {
-    return a + b;
-}
-const result = console.log(add(2, 2));
